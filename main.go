ce15ee17108dcf4ca7b5eb473833962eaccfacf3
Vladislav Kishkovsky <67899905+aDeepRecession@users.noreply.github.com>
2023-01-29 16:38:29 +0300

    chore: change main to check grades every 10 minutes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	gradeshistory "github.com/aDeepRecession/moodle-scrapper/pkg/gradesHistory"
 	moodleapi "github.com/aDeepRecession/moodle-scrapper/pkg/moodleAPI"
@@ -10,6 +11,8 @@ import (
 )
 
 var logger *log.Logger = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+
+var checkIntervalDuraion = time.Minute * 10
 
 func main() {
 	tokens, err := moodletokensmanager.GetTokens()
@@ -25,13 +28,45 @@ func main() {
 		os.Exit(1)
 	}
 
-	logger.Println("getting moodle courses...")
-	courses, err := moodleAPI.GetCourses()
-	if err != nil {
-		logger.Println(err)
-		os.Exit(1)
-	}
+	for {
+		logger.Println("getting moodle courses...")
+		courses, err := moodleAPI.GetCourses()
+		if err != nil {
+			logger.Println(err)
+			os.Exit(1)
+		}
 
+		coursesGrades := getGradesForNonHiddenCourses(moodleAPI, courses)
+
+		saveCfg := gradeshistory.SaveConfig{
+			LastGradesPath:    "./last_grades.json",
+			GradesHistoryPath: "./grades_history.json",
+		}
+		gradesHistory := gradeshistory.NewGradesHistory(saveCfg, logger)
+
+		err = gradesHistory.UpdateGradesHistory(coursesGrades)
+		if err != nil {
+			logger.Println(err)
+			os.Exit(1)
+		}
+
+		logger.Printf(
+			"Waiting... next check at %q",
+			getNextCheckTime(checkIntervalDuraion).Format(time.Layout),
+		)
+
+		time.Sleep(checkIntervalDuraion)
+	}
+}
+
+func getNextCheckTime(sleepDuration time.Duration) time.Time {
+	return time.Now().Add(sleepDuration)
+}
+
+func getGradesForNonHiddenCourses(
+	moodleAPI moodleapi.MoodleAPI,
+	courses []moodleapi.Course,
+) []gradeshistory.CourseGrades {
 	coursesGrades := []gradeshistory.CourseGrades{}
 	for _, course := range courses {
 		if course.Hidden {
@@ -49,16 +84,5 @@ func main() {
 			Grades: grades,
 		})
 	}
-
-	saveCfg := gradeshistory.SaveConfig{
-		LastGradesPath:    "./last_grades.json",
-		GradesHistoryPath: "./grades_history.json",
-	}
-	gradesHistory := gradeshistory.NewGradesHistory(saveCfg, logger)
-
-	err = gradesHistory.UpdateGradesHistory(coursesGrades)
-	if err != nil {
-		logger.Println(err)
-		os.Exit(1)
-	}
+	return coursesGrades
 }
